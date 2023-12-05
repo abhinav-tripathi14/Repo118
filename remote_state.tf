e5caf62f4289d7a2cb3ab64294c9348a6f0dc3f6
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-12-05 14:27:22 +0530

    Add files via upload

---
+++ remote_state.tf
@@ -0,0 +1,32 @@
+data "terraform_remote_state" "network" {
+  backend = "remote"
+    config = {
+      organization = "mediado"
+
+      workspaces = {
+      name = "ebook_dev_network"
+    }
+  }
+}
+
+data "terraform_remote_state" "ebook_file_upload_service_backend" {
+  backend = "remote"
+    config = {
+      organization = "mediado"
+
+      workspaces = {
+      name = "ebook_dev_app_ebook_file_upload_service_backend"
+    }
+  }
+}
+
+data "terraform_remote_state" "strage" {
+  backend = "remote"
+    config = {
+      organization = "mediado"
+
+      workspaces = {
+      name = "ebook_dev_strage"
+    }
+  }
+}
