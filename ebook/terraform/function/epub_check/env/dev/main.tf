5c4f3e9c327ec19e848149c9ce6e88128b0156fb
Abhinav Tripathi <47190942+abhinav-tripathi14@users.noreply.github.com>
2023-12-19 09:30:46 +0530

    Update main.tf

--- ebook/terraform/function/epub_check/env/dev/main.tf
+++ ebook/terraform/function/epub_check/env/dev/main.tf
@@ -1,25 +1,3 @@
-data "aws_caller_identity" "self" {}
-data "aws_region" "current" {}
-
-module "iam" {
-  source     = "../../modules/iam"
-  env        = var.env
-  service    = var.service
-  account_id = data.aws_caller_identity.self.account_id
-
-}
-
-module "lambda" {
-  source                = "../../modules/lambda"
-  env                   = var.env
-  service               = var.service
-  role_arn              = module.iam.lambda_role_arn
-  efs_ap_arn            = data.terraform_remote_state.strage.outputs.efs_lambda_ap_arn
-  queue_arn             = data.terraform_remote_state.ebook_file_upload_service_backend.outputs.epub_check_queue_arn
-  send_sqs_url          = data.terraform_remote_state.ebook_file_upload_service_backend.outputs.opf_get_queue_url
-  app_security_group_id = data.terraform_remote_state.network.outputs.internal_app_security_group_id
-  pub_sub_a_id          = data.terraform_remote_state.network.outputs.pub_sub_a_id
-  pub_sub_c_id          = data.terraform_remote_state.network.outputs.pub_sub_c_id
-  account_id            = data.aws_caller_identity.self.account_id
-  region                = data.aws_region.current.name
+resource "null_resource" "abhinav"{
+count=2
 }
